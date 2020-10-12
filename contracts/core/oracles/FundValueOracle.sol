6fdb4cade05b491851b208cb2d1e98912da733bd
Ruslan <ruslan777mirov@gmail.com>
2020-10-12 12:31:21 +0300

    Update Oracle

--- contracts/core/oracles/FundValueOracle.sol
+++ contracts/core/oracles/FundValueOracle.sol
@@ -5,31 +5,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 
 
-library Strings {
-    function concat(string memory _base, string memory _value) internal pure returns (string memory) {
-        bytes memory _baseBytes = bytes(_base);
-        bytes memory _valueBytes = bytes(_value);
-
-        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
-        bytes memory _newValue = bytes(_tmpValue);
-
-        uint i;
-        uint j;
-
-        for(i=0; i<_baseBytes.length; i++) {
-            _newValue[j++] = _baseBytes[i];
-        }
-
-        for(i=0; i<_valueBytes.length; i++) {
-            _newValue[j++] = _valueBytes[i++];
-        }
-        return string(_newValue);
-    }
-}
-
 contract FundValueOracle is ChainlinkClient, Ownable{
-    using Strings for string;
-
     string public apiPath;
     bytes32[] public requestIdArrays;
     uint256 public fee;
@@ -67,7 +43,7 @@ contract FundValueOracle is ChainlinkClient, Ownable{
 
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
-        string memory path = apiPath.concat(addressToString(_fundAddress));
+        string memory path = string(abi.encodePacked(apiPath, addressToString(_fundAddress)));
 
         // Set the URL to perform the GET request on
         request.add("get", path);
@@ -75,6 +51,12 @@ contract FundValueOracle is ChainlinkClient, Ownable{
 
         // Sends the request
         return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+
+    function checkPath(address _fundAddress) public view returns(string memory){
+        string memory path = string(abi.encodePacked(apiPath, addressToString(_fundAddress)));
+        return path;
     }
 
     /**
@@ -87,12 +69,12 @@ contract FundValueOracle is ChainlinkClient, Ownable{
       requestIdArrays.push(_requestId);
     }
 
-    // owne can update api endpoint
+    // owner can update api endpoint
     function updateApiPath(string calldata _apiPath) external onlyOwner {
       apiPath = _apiPath;
     }
 
-    // owne can update fee
+    // owner can update fee
     function updateFee(uint256 _fee) external onlyOwner {
       fee = _fee;
     }
