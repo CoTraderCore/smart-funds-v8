54e1ed1649b73a65c3fe6c3316ebd02a27111161
Ruslan <ruslan777mirov@gmail.com>
2020-10-29 09:02:56 +0200

    Add ICoTraderGlobalConfig

---
+++ contracts/core/interfaces/ICoTraderGlobalConfig.sol
@@ -0,0 +1,10 @@
+interface ICoTraderGlobalConfig {
+  function MIN_TRADE_FREEZE() external view returns(uint256);
+  function MAX_TRADE_FREEZE() external view returns(uint256);
+
+  function MIN_DW_INTERVAL() external view returns(uint256);
+  function MAX_DW_INTERVAL() external view returns(uint256);
+
+  function MIN_MAX_TOKENS() external view returns(uint256);
+  function MAX_MAX_TOKENS() external view returns(uint256);
+}
